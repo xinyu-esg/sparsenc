5bc4ac0c2e121abb07b227049b1d1da5486f2505
Ye Li <leeyee.seu@gmail.com>
2015-09-17 09:35:04 -0400

    Move internal header files from include to src

---
+++ src/bipartite.h
@@ -0,0 +1,21 @@
+/*---------------------- bipartite.h -------------------
+ * Internal header file
+ *------------------------------------------------------*/
+#ifndef BIPARTITE_H
+#define BIPARTITE_H
+#include "common.h"
+typedef struct node      NBR_node;
+typedef struct node_list NBR_nodes;
+
+// Bipartitle graph for LDPC code
+typedef struct bipartite_graph {
+	int		  nleft;
+	int		  nright;
+	NBR_nodes **l_nbrs_of_r;							// left side neighbours of right
+	NBR_nodes **r_nbrs_of_l;							// right side neighbours of left					
+} BP_graph;
+
+int number_of_checks(int snum); 
+void create_bipartite_graph(BP_graph *graph, int nleft, int nright);
+void free_bipartite_graph(BP_graph *graph);
+#endif /* BIPARTITE_H */
---
+++ src/galois.h
@@ -0,0 +1,21 @@
+/*-------------------- galois.h ------------------------------
+ *
+ * Internal header file of Galois field implementation
+ *
+ *-----------------------------------------------------------*/
+#ifndef GALOIS_H
+#define GALOIS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#define GF_POWER	8			/* Order of Galois field: 2^8, don't change it */
+typedef unsigned char GF_ELEMENT;
+// Galois field arithmetic routines
+int constructField(int npower);
+uint8_t galois_add(uint8_t a, uint8_t b);
+uint8_t galois_sub(uint8_t a, uint8_t b);
+uint8_t galois_multiply(uint8_t a, uint8_t b, int npower);
+uint8_t galois_divide(uint8_t a, uint8_t b, int npower);
+void galois_multiply_region(uint8_t *src, uint8_t multiplier, int bytes, int npower);
+void galois_multiply_add_region(uint8_t *dst, uint8_t *src, uint8_t multiplier, int bytes, int npower);
+#endif
